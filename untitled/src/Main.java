7dd1f2e8271fb7f2a858574056f089e5bb87fff7
jujihun1 <wnwlgns9295@gmail.com>
2022-09-22 13:12:54 +0900

    study

---
+++ untitled/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 주석 ** : 컴퓨터가 인식하지 못하는 영역
+
+
+        /*
+        * 주석 : 여러줄의 영역을 인식하지 못하게 해야할 경우에 사용
+        */
+        System.out.println("Hello world!");
+        System.out.println("주지훈");
+        System.out.println("주지훈");
+    }
+}
