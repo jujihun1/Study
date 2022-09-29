9561dc153d1e894eb603dab71bac489f9ca0d6b1
jujihun1 <wnwlgns9295@gmail.com>
2022-09-29 13:26:28 +0900

    Step_02

--- untitled/Step_01/src/Main.java
+++ untitled/Step_01/src/Main.java
@@ -91,7 +91,62 @@ public class Main {
              */
 
             // ** 비트 연산자
+            // [~] = 반전 연산자
+            // (NOT 연산자)
+            {
+                int number = -10;
+                System.out.println(~number);
+            }
+            // [^] = 비트가 서로 다른상태 일때 true
+            // (XOR 연산자)
+            {
+                int number1 = 1;    //  00000001
+                int number2 = 3;  //  ^ 00000010
+                                    //  00000011
+                System.out.println(number1 ^ number2);
+            // 변수 생성x (^)XOR 연산자로 스왑
+                number1 = number1 ^ number2;
+                number2 = number2 ^ number1;
+                number1 = number1 ^ number2;
+
+                System.out.println("number1 : " + number1);
+                System.out.println("number2 : " + number2);
+
+            }
+
+            //[<<], [>>], [>>>]
+            {
+                // 2 - 1 = 1
+                // 2 + (-1) = 1
+
+                // 5 - 10 = -10
+                // 5 + (-10) = -5
+
+                // 만약 어떤 값에 절반에 해당하는 값으로 초기화 해야하는 경우
+                // int x = 10/2;
+
+                // 나누기 연산자를 사용하는 것보다 곱하기 연산자를 사용하는 것이 효율적이다
+                // int x = 10/0.5f;
+
+                // 곱하기 연산자를 ㅏ용하는 것보다 쉬프트 연산자를 사용하는 것이 효율적이다
+                int i = 10>>1;
+                System.out.println(i);
+
+                // ** [<<] = *2 와 같음
+                // ** [>>] = /2 와 같음
+
+                // [>>>] = 부호 변경 용도
+            }
+
             // ** 삼항 연산자
+            {
+                //조건식 ? true : false;
+
+                int x = 1000;
+
+                x = 100 < x ? 100 : x;
+                System.out.println("x : " + x);
+            }
             // ** 문자열 연산자 +
 
             System.out.println("문자열" + "출력");
