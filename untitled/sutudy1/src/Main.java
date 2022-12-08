79f5146c5865b7e808ffd50bd9e04598db83c2d7
jujihun1 <wnwlgns9295@gmail.com>
2022-12-08 13:22:30 +0900

    12-08

---
+++ untitled/sutudy1/src/Main.java
@@ -0,0 +1,55 @@
+public class Main {
+
+
+    public  String  Member (){
+        return "";
+    }
+
+    public int MemberA(){
+        return 10;
+    }
+
+
+    public static void main(String[] args) {
+
+
+//        연산자
+//        산술연산자
+//        +, -, *,/, %, ( = 대입연산자)
+
+//        비교연산자
+//        <, >, <=, >=
+
+//        논리연산자
+//        ==, !=, ! &&, ||, &, |
+
+//        분기문(예외처리)
+//        if ~else, switch ~ case
+
+//        반복문
+//        for, while
+
+//        배열
+//        int[] array = new int[0];
+//        c 언어 : int array[0];
+
+//        {
+//        scope 
+//        영역
+//        영역
+//        영역
+//        }
+
+//        method  > void 의경우 반환값 x
+//        method 자료형 값과 return 값과 같아야한다.  int inta(){ return 10;}
+//        method 자료형 값과 return 값과 같아야한다.  double doubleA(){ return 0.0;}
+//        method 자료형 값과 return 값과 같아야한다.  float floatA(){ return 0.0f;}
+//        method 자료형 값과 return 값과 같아야한다.  String stringA(){ return "str";}
+//        method 자료형 값과 return 값과 같아야한다.  Member member(){ return member;}
+
+//        L value  R value
+//         변수       상수
+//        int i = 10;
+
+    }        
+}
